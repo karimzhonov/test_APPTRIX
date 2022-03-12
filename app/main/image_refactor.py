2f5d307d74ff7117540e787e83417bca6bf53915
Khusniddin <khtkarimzhonov@gmail.com>
2022-03-13 00:41:47 +0300

    fixes

--- app/main/image_refactor.py
+++ app/main/image_refactor.py
@@ -25,23 +25,27 @@ from django.conf import settings
 #     return image
 
 def impose_ico_to_image(avatar):
-    image = np.array(Image.open(io.BytesIO(avatar.read())))
-    image_height, image_wight, _ = image.shape
+    # Avatar
+    image = Image.open(io.BytesIO(avatar.read()))
+    image_height, image_wight = image.size
+    # Ico
     path_to_ico = os.path.join(settings.STATIC_ROOT, 'favicon.jpeg')
-    ico = np.array(Image.open(path_to_ico))
-    ico_height, ico_width, _ = ico.shape
+    ico = Image.open(path_to_ico)
+    ico_height, ico_width = ico.size
     # refactoring ico
     new_ico_width = image_wight // 10
     new_ico_height = int(ico_height * (new_ico_width / ico_width))
-    ico.resize((new_ico_width, new_ico_height, 3))
+    ico = ico.resize((new_ico_height, new_ico_width))
     # Impose ico to image
     top = image_height // 20
     left = image_wight // 20
-    image[top:top + new_ico_height, left:left + new_ico_width] = ico
+    image_np = np.array(image)
+    ico_np = np.array(ico)
+    image_np[top:top + new_ico_height, left:left + new_ico_width] = ico_np
 
     image_name = avatar.name
     _format = image_name.split('.')[-1]
     buf = io.BytesIO()
-    new_image = Image.fromarray(image)
+    new_image = Image.fromarray(image_np)
     new_image.save(buf, format='JPEG')
     return buf.getvalue()
